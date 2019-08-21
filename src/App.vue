1c0ba2cd46377bf7d2471c2b2517894e601929bf
Sergei Kossik <sergei.kossik@consultant.swedbank.ee>
2019-08-21 13:36:18 +0300

    v-slot directive not fully supported bug

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container>
+      <b-table :fields="fields" :items="items" foot-clone>
+        <!-- THIS IS BROKEN -->
+        <template v-slot:[name]="data">
+          {{ data.value.first }} {{ data.value.last }}
+        </template>
+        <!-- THIS WORKS NICELY -->
+        <!-- <template slot="[name]" slot-scope="data">
+          {{ data.value.first }} {{ data.value.last }}
+        </template> -->
+
+        <!-- A custom formatted header cell for field 'name' -->
+        <template v-slot:HEAD[name]="data">
+          <span class="text-info"
+            ><b>{{ data.label }}</b></span
+          >
+        </template>
+
+        <!-- A custom formatted footer cell for field 'name' -->
+        <template slot="FOOT[name]" slot-scope="data">
+          <span class="text-danger">{{ data.label }}</span>
+        </template>
+
+        <!-- Default fall-back custom formatted footer cell -->
+        <template slot="FOOT[]" slot-scope="data">
+          <i>{{ data.label }}</i>
+        </template>
+ 
+       <!-- WORKS-->
+        <!-- <template slot="[]">
+          Override everything
+        </template>  -->
+
+        <!-- NOT WORKING with v-slot directive -->
+        <!-- <template v-slot:[]>
+          Override everything
+        </template> -->
+      </b-table>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      // name: 'name',
+      fields: [
+        // A column that needs custom formatting
+        { key: "name", label: "Full Name" },
+        // A regular column
+        "age",
+        // A regular column
+        "sex"
+      ],
+      items: [
+        { name: { first: "John", last: "Doe" }, sex: "Male", age: 42 },
+        { name: { first: "Jane", last: "Doe" }, sex: "Female", age: 36 },
+        { name: { first: "Rubin", last: "Kincade" }, sex: "Male", age: 73 },
+        {
+          name: { first: "Shirley", last: "Partridge" },
+          sex: "Female",
+          age: 62
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
